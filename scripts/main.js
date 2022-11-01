71caf483604ce9cf3c41eac37fdde66c3186d361
ItsJustKon <jrmarshawn@gmail.com>
2022-10-31 20:22:37 -0400

    start fronyt entd

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,16 @@
 // tbh you can just set up a sever or whatever you did kinda like what you did with leostats
 // and just convert the csv to json and store it please Kingston
-
+var namediv = document.getElementById("name")
+var agediv = document.getElementById("age")
+var racediv = document.getElementById("race")
+var genderdiv = document.getElementById("gender")
+var statediv = document.getElementById("state")
+var indicatordiv = document.getElementById("indicator")
+var obejectsarray = [namediv, agediv, racediv, genderdiv, statediv, indicatordiv]
+var questionsdiv = document.getElementById("currentquestion")
+var index = 1
+var backbutton = document.createElement("button")
+backbutton.style.display = "none"
 function infosubmit()
 {
  
@@ -12,6 +22,27 @@ function infosubmit()
     var indicator = document.getElementById("indicator").value
     console.log(`your name is ${name}`)
     calculateprobs(age, race, gender, state, indicator)
+}
+
+function nextquestion()
+{
+    index = index + 1
+    if(backbutton.style.display == "none")
+    {
+        console.log("TRUE")
+        if(index >= 2)
+        {
+            backbutton.style.display = ""
+        }
+    }
+
+    console.log(backbutton.style.display)
+}
+
+function backquestion()
+{
+    index = index - 1
+
 }
 
 function calculateprobs(age, race, gender, state, indicator)
