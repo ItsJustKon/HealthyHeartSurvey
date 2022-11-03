1f3381a021a36f7c4c8111607b73d4b3da68727f
kingstonv1 <kingstongise@gmail.com>
2022-11-03 14:11:05 -0400

    back button works!

--- scripts/main.js
+++ scripts/main.js
@@ -6,32 +6,38 @@ let questions = document.getElementById("currentQuestion");
 let backbutton = document.getElementById("backButton");
 let nextButton = document.getElementById("nextButton");
 let questionsList = ["age", "gender", "race", "height", "weight", "state"];
-let index = 0;
+let index = -1;
 
 //backbutton.style.display = "none";
 
 function backQuestion() {
-    index--;
-    questions.innerHTML = "";
 
-    console.log(index);
-    questions.appendChild(document.getElementById(questionsList[index]));
+    if (index > 0) {
+        index--;
+        questions.innerHTML = "";
+
+        questions.appendChild(document.getElementById(questionsList[index]).cloneNode(true));
+    }
 };
 
 function nextQuestion() {
-    questions.innerHTML = "";
     
-    //Gets one question from questions list based on the index.
-    //This prevents a lot of declarations at the top of the file.
-    console.log(index);
-    questions.appendChild(document.getElementById(questionsList[index]));
-    
-    //I'm assuming that we will have 12 questions.
-    if (index === 12) {
-        nextButton.innerHTML = "Submit";
-    }
+    //Change this number when we add more questions.
+    if (index <= 5) {
+        index++;
+        questions.innerHTML = "";
 
-    index++;
+        //Gets one question from questions list based on the index.
+        //This prevents a lot of declarations at the top of the file.
+        //We clone the node because if we don't, the element is deleted and cannot be used again.
+        questions.appendChild(document.getElementById(questionsList[index]).cloneNode(true));
+        
+        //I'm assuming that we will have 12 questions.
+        if (index === 12) {
+            nextButton.innerHTML = "Submit";
+        }
+
+    }
 };
 
 
