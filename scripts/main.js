c514d6fd61ec68fc3acbf8ca5e65b4226ca134b8
kingstonv1 <kingstongise@gmail.com>
2022-11-03 13:39:18 -0400

    working nextQuestion()

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,41 @@
 // tbh you can just set up a sever or whatever you did kinda like what you did with leostats
 // and just convert the csv to json and store it please Kingston
-let namediv = document.getElementById("name");
-let agediv = document.getElementById("age");
-let racediv = document.getElementById("race");
-let genderdiv = document.getElementById("gender");
-let statediv = document.getElementById("state");
-let indicatordiv = document.getElementById("indicator");
-let questionsdiv = document.getElementById("currentquestion");
-let backbutton = document.createElement("backButton");
+let name = document.getElementById("name");
+let indicator = document.getElementById("indicator");
+let questions = document.getElementById("currentQuestion");
+let backbutton = document.getElementById("backButton");
 let nextButton = document.getElementById("nextButton");
-let obejectsarray = [namediv, agediv, racediv, genderdiv, statediv, indicatordiv];
-let index = 1;
+let questionsList = ["age", "gender", "race", "height", "weight", "state"];
+let index = 0;
 
-backbutton.style.display = "none";
+//backbutton.style.display = "none";
+
+function backQuestion() {
+    index--;
+    questions.innerHTML = "";
+
+    console.log(index);
+    questions.appendChild(document.getElementById(questionsList[index]));
+};
+
+function nextQuestion() {
+    questions.innerHTML = "";
+    
+    //Gets one question from questions list based on the index.
+    //This prevents a lot of declarations at the top of the file.
+    console.log(index);
+    questions.appendChild(document.getElementById(questionsList[index]));
+    
+    //I'm assuming that we will have 12 questions.
+    if (index === 12) {
+        nextButton.innerHTML = "Submit";
+    }
+
+    index++;
+};
+
+
 function infosubmit() {
-
     let name = document.getElementById("name").value;
     let age = document.getElementById("age").value;
     let race = document.getElementById("race").value;
@@ -22,31 +43,5 @@ function infosubmit() {
     let state = document.getElementById("state")
     let indicator = document.getElementById("indicator").value;
     console.log(`your name is ${name}`);
-    calculateprobs(age, race, gender, state, indicator);
 };
 
-function nextquestion() {
-    index = index + 1;
-    //I'm assuming that we have 12 questions.
-    if (index === 12) {
-
-    }
-
-    if (backbutton.style.display == "none") {
-        console.log("TRUE");
-        if (index >= 2) {
-            backbutton.style.display = "";
-        }
-    }
-
-    console.log(backbutton.style.display);
-};
-
-function backquestion() {
-    index = index - 1;
-};
-
-function calculateprobs(age, race, gender, state, indicator) {
-    // probibilty of getting heart diease or whatever = a whole lotta math or something like that y'know
-    return probibilty;
-};
