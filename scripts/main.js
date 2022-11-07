81d1650305a6fd646a8472dc931002a1f8648a1f
kingstonv1 <kingstongise@gmail.com>
2022-11-06 19:36:57 -0500

    parse answers into JSON object

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,35 @@
 let questions = document.getElementById("currentQuestion");
 let backButton = document.getElementById("backButton");
 let nextButton = document.getElementById("nextButton");
-let questionsList = ["age", "gender", "race", "height", "weight", "state", "diabetes", "smoking", "cholesterol", "excercise"];
-let answers = {age: "", gender: "", "race": "", height: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: "" };
+let questionsList = ["age", "gender", "race", "feet", "inches", "weight", "state", "diabetes", "smoking", "cholesterol", "excercise"];
+let answers = {age: "", gender: "", "race": "", feet: "", inches: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: ""};
 let index = -1;
 
-backButton.style.opacity = 0;
 
-function getValue(index) {
-    let value;
-    let element= document.getElementById(questionsList[index]);
-
-    // if () 
-    // {}
-
-
-    return value;
+function storeValue(index) {
+    if (index != -1) {
+        answers[questionsList[index]] = document.getElementById(questionsList[index]).value;
+    }
 }
 
 function backQuestion() {
-
     if (index > 0) {
+        storeValue(index);
+
         index--;
         questions.innerHTML = "";
         
-        let current = questionsList[index];
-        answers.current = document.getElementById(current);
-        
-
-        questions.appendChild(document.getElementById(current).cloneNode(true));
-
-        if (index === 0) {
-            backButton.style.opacity = 0;
+        if (index === 3) {
+            questions.appendChild(document.getElementById(questionsList[index]).cloneNode(true));
+            questions.appendChild(document.getElementById(questionsList[index + 1]).cloneNode(true));
+            index++;
         }
+        else {
+            questions.appendChild(document.getElementById(questionsList[index]).cloneNode(true));
+        }
+        
+        if (index === 0) 
+        { backButton.style.opacity = 0; }
 
         nextButton.innerHTML = "Next Question";
     }
@@ -41,19 +38,11 @@ function backQuestion() {
 function nextQuestion() {
     
     if (index < (questionsList.length - 1)) {
+        storeValue(index);
         
-        try {
-            let current = questionsList[index];
-            
-            answers.current = document.getElementById(current).value;
-    
-            questions.innerHTML = "";
-        }
-        catch (e) { console.log(e); }
-
         index++;
         questions.innerHTML = "";
-
+        
         //Gets one question from questions list based on the index.
         //This prevents a lot of declarations at the top of the file.
         //We clone the node because if we don't, the element is deleted and cannot be used again.
@@ -65,17 +54,15 @@ function nextQuestion() {
         }
         
         backButton.style.opacity = 1;
+        
     }
 };
 
-
 function infoSubmit() {
-    let name = document.getElementById("name").value;
-    let age = document.getElementById("age").value;
-    let race = document.getElementById("race").value;
-    let gender = document.getElementById("gender").value;
-    let state = document.getElementById("state").value;
-    
-    console.log(name, age, race, gender, state);
+    console.log(answers);
+
+    //Send a POST request to localhost://8080 with all of the answers included as a form.
 };
 
+backButton.style.opacity = 0;
+nextQuestion();
