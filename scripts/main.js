055291e0f73cfd4aa12412008674a7a3e305bd40
Marshawn <jrmarshawn@gmail.com>
2022-11-10 10:52:03 -0500

    Fixed Frontend

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,23 @@
 let questions = document.getElementById("currentQuestion");
+let labels = document.getElementById("currentLabel")
+let labelsList = document.getElementById("labelsList")
 let backButton = document.getElementById("backButton");
 let nextButton = document.getElementById("nextButton");
-let questionsList = ["age", "gender", "race", "feet", "inches", "weight", "state", "diabetes", "smoking", "cholesterol", "excercise"];
-let answers = {age: "", gender: "", "race": "", feet: "", inches: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: ""};
+let questionsList = document.getElementById("questions")
+let answers = {age: "", gender: "", race: "", height: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: ""};
 let index = -1;
-
 
 function storeValue(index) {
     if (index != -1) {
-        answers[questionsList[index]] = document.getElementById(questionsList[index]).value;
+        if (index === 3)
+        {
+            answers[questionsList[index]] = `Feet:${document.getElementById("feet").value},Inches:${document.getElementById("inches").value}`
+        }
+        else
+        {
+            answers[questionsList[index]] = document.getElementById(questionsList.children[0].id).value;
+        }
+        
     }
 }
 
@@ -18,37 +27,46 @@ function backQuestion() {
 
         index--;
         questions.innerHTML = "";
+        labels.innerHTML = ""
         
         if (index === 3) {
-            questions.appendChild(document.getElementById(questionsList[index]).cloneNode(true));
-            questions.appendChild(document.getElementById(questionsList[index + 1]).cloneNode(true));
-            index++;
+            questions.appendChild(document.getElementById("feet").cloneNode(true));
+            questions.appendChild(document.getElementById("inches").cloneNode(true));
         }
         else {
-            questions.appendChild(document.getElementById(questionsList[index]).cloneNode(true));
+            questions.appendChild(questionsList.children[index].cloneNode(true));
         }
-        
+        labels.appendChild(labelsList.children[index].cloneNode(true))
         if (index === 0) 
         { backButton.style.opacity = 0; }
 
         nextButton.innerHTML = "Next Question";
+        nextButton.removeEventListener('click', infoSubmit)
     }
 };
 
 function nextQuestion() {
-    
-    if (index < (questionsList.length - 1)) {
+    if (index < (questionsList.children.length - 1)) {
         storeValue(index);
         
         index++;
         questions.innerHTML = "";
+        labels.innerHTML = ""
         
         //Gets one question from questions list based on the index.
         //This prevents a lot of declarations at the top of the file.
         //We clone the node because if we don't, the element is deleted and cannot be used again.
-        questions.appendChild(document.getElementById(questionsList[index]).cloneNode(true));
+        if (index === 3) {
+            questions.appendChild(document.getElementById("feet").cloneNode(true));
+            questions.appendChild(document.getElementById("inches").cloneNode(true));
+        }
+        else {
+            questions.appendChild(questionsList.children[index].cloneNode(true));
+            console.log("APPEND")
+        }
+        labels.appendChild(labelsList.children[index].cloneNode(true))
         
-        if (index === questionsList.length - 1) {
+        if (index === questionsList.children.length - 1) {
             nextButton.innerHTML = "Submit";
             nextButton.addEventListener('click', infoSubmit)
         }
