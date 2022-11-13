dd5751978b9b61a788bc8389e20cf403c5465512
kingstonv1 <kingstongise@gmail.com>
2022-11-12 23:57:20 -0500

    Send data using POST Form

--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,20 @@ let labelsList = document.getElementById("labelsList")
 let backButton = document.getElementById("backButton");
 let nextButton = document.getElementById("nextButton");
 let questionsList = document.getElementById("questions")
-let answers = {age: "", gender: "", race: "", height: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: ""};
+let answers = {age: "", gender: "", race: "", heightFt: "", heightIn: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: ""};
 let index = -1;
 
 function storeValue(index) {
     if (index != -1) {
         if (index === 3)
         {
-            answers[questionsList[index]] = `Feet:${document.getElementById("feet").value},Inches:${document.getElementById("inches").value}`
+            answers["heightFt"] = `Feet:${document.getElementById("feet").value}`
+            answers["heightIn"] = `${document.getElementById("inches").value}`
         }
         else
         {
-            answers[questionsList[index]] = document.getElementById(questionsList.children[0].id).value;
+            answers[questionsList.children[index].id] = document.getElementById(questionsList.children[index].id).value;
         }
-        
     }
 }
 
@@ -36,7 +36,9 @@ function backQuestion() {
         else {
             questions.appendChild(questionsList.children[index].cloneNode(true));
         }
+        
         labels.appendChild(labelsList.children[index].cloneNode(true))
+        
         if (index === 0) 
         { backButton.style.opacity = 0; }
 
@@ -62,7 +64,6 @@ function nextQuestion() {
         }
         else {
             questions.appendChild(questionsList.children[index].cloneNode(true));
-            console.log("APPEND")
         }
         labels.appendChild(labelsList.children[index].cloneNode(true))
         
@@ -75,11 +76,29 @@ function nextQuestion() {
         
     }
 };
+//let answers = {age: "", gender: "", race: "", heightFt: "", heightIn: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: ""};
 
-function infoSubmit() {
+async function infoSubmit() {
     console.log(answers);
 
     //Send a POST request to localhost://8080 with all of the answers included as a form.
+    let res = await axios.post("http://localhost:8080", {
+        age: answers.age,
+        gender: answers.gender,
+        race: answers.race,
+        heightFt: answers.heightFt,
+        heightIn: answers.heightIn,
+        weight: answers.weight,
+        state: answers.state,
+        diabetes: answers.diabetes,
+        smoking: answers.smoking,
+        cholesterol: answers.cholesterol,
+        excercise: answers.excercise
+    })
+    .then(function (res) {console.log(res.data);})
+    .catch(function (err) {console.log(err);});
+
+
 };
 
 backButton.style.opacity = 0;
--- srv/main.py
+++ srv/main.py
@@ -1,6 +1,8 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import cgi
 import csv
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+count = 0
 
 with open('../data/heartData.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
@@ -9,37 +11,48 @@ with open('../data/heartData.csv', mode='r') as csv_file:
 
 overall = []
 
-for i in range(len(rows)):
-    #Fetches the first instance of average smoking risk
-    if rows[i]['locationabbr'] == "USM" and rows[i]['topic'] == "Smoking" and rows[i]['break_out'] == "Overall":
-        overall.append(i)
-        break
+# for i in range(len(rows)):
+#     #Fetches the first instance of average smoking risk
+#     if rows[i]['locationabbr'] == "USM" and rows[i]['topic'] == "Smoking" and rows[i]['break_out'] == "Overall":
+#         overall.append(i)
+#         break
 
-print(overall)
-print(rows[0])
 
-hostName = "localhost"
-serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST')
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+    
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("Hello!", "utf-8"))
+        global count
+        count += 1
+        print("Request #" + str(count) + " fulfilled.")
+        
     
     def do_POST(self):
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST')
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
         
         form = cgi.FieldStorage(
-            fp = self.rfile,
-            headers = self.headers,
-            environ = {"REQUEST_METHOD": "POST"}
-        )        
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST'} )
         
-        # print(form.getvalue('age'))
+        print(form)
 
+hostName = "localhost"
+serverPort = 8080
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
