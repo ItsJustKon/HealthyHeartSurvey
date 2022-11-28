64df9e4b1739a0f3a0675149e6b11513137d2c99
kingstonv1 <kingstongise@gmail.com>
2022-11-28 09:19:35 -0500

    event listeners in JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,124 +1,109 @@
 let questions = document.getElementById("currentQuestion");
-let labels = document.getElementById("currentLabel")
-let labelsList = document.getElementById("labelsList")
+let labels = document.getElementById("currentLabel");
+let labelsList = document.getElementById("labelsList");
 let backButton = document.getElementById("backButton");
 let nextButton = document.getElementById("nextButton");
-let questionsList = document.getElementById("questions")
+let questionsList = document.getElementById("questions");
 let answers = { age: "", gender: "", race: "", heightFt: "", heightIn: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: "" };
 let index = -1;
 
-function storeValue(index) {
-    if (index != -1) {
-        if (index === 3) {
-            answers["heightFt"] = `Feet:${document.getElementById("feet").value}`
-            answers["heightIn"] = `${document.getElementById("inches").value}`
-        }
-        else {
-            answers[questionsList.children[index].id] = document.getElementById(questionsList.children[index].id).value;
-        }
-    }
+async function recallValues() {
+	if (Object.values(answers)[index] != "") {
+		questions.childNodes[0].value = Object.values(answers)[index];
+		if (index === 3) {
+			questions.childNodes[1].value = answers["heightIn"];
+		}
+	}
 }
 
+async function hideBackButton() {
+	backButton.style.opacity = 0;
+}
+
+async function revealBackButton() {
+	backButton.style.opacity = 1;
+}
+
+function storeValue(index) {
+	if (index != -1) {
+		if (index === 3) {
+			answers["heightFt"] = `${document.getElementById("feet").value}`;
+			answers["heightIn"] = `${document.getElementById("inches").value}`;
+		}
+		else {
+			answers[questionsList.children[index].id] = document.getElementById(questionsList.children[index].id).value;
+		}
+	}
+}
+
+async function nextToSubmit() {
+	nextButton.innerHTML = "Submit";
+	nextButton.addEventListener("click", infoSubmit);
+}
+
+async function submitToNext() {
+	nextButton.innerHTML = "Next Question";
+	nextButton.removeEventListener("click", infoSubmit);
+}
+
+function wipeQuestions() {
+	questions.innerHTML = "";
+	labels.innerHTML = "";
+}
+
+function updateQuestion() {
+	//Gets one question from questions list based on the index.
+	//This prevents a lot of declarations at the top of the file.
+	//We clone the node because if we don't, the element is deleted and cannot be used again.
+	if (index === 3) {
+		questions.appendChild(document.getElementById("feet").cloneNode(true));
+		questions.appendChild(document.getElementById("inches").cloneNode(true));
+	} else {
+		questions.appendChild(questionsList.children[index].cloneNode(true));
+	}
+
+	labels.appendChild(labelsList.children[index].cloneNode(true));
+}
+
+
+
 function backQuestion() {
-    if (index > 0) {
-        storeValue(index);
-
-        index--;
-        questions.innerHTML = "";
-        labels.innerHTML = ""
-
-        if (index === 3) {
-            questions.appendChild(document.getElementById("feet").cloneNode(true));
-            questions.appendChild(document.getElementById("inches").cloneNode(true));
-        }
-        else {
-            questions.appendChild(questionsList.children[index].cloneNode(true));
-        }
-
-        labels.appendChild(labelsList.children[index].cloneNode(true))
-
-        if (index === 0) { backButton.style.opacity = 0; }
-
-        nextButton.innerHTML = "Next Question";
-        nextButton.removeEventListener('click', infoSubmit)
-    }
-};
+	if (index > 0) {
+		storeValue(index);
+		
+		index--;
+		
+		wipeQuestions();
+		updateQuestion();
+		hideBackButton();
+		recallValues();
+		submitToNext();
+		
+	}
+}
 
 function nextQuestion() {
-    if (index < (questionsList.children.length - 1)) {
-        storeValue(index);
+	if (index < (questionsList.children.length - 1)) {
+		storeValue(index);
 
-        index++;
-        questions.innerHTML = "";
-        labels.innerHTML = ""
-
-        //Gets one question from questions list based on the index.
-        //This prevents a lot of declarations at the top of the file.
-        //We clone the node because if we don't, the element is deleted and cannot be used again.
-        if (index === 3) {
-            questions.appendChild(document.getElementById("feet").cloneNode(true));
-            questions.appendChild(document.getElementById("inches").cloneNode(true));
-        }
-        else {
-            questions.appendChild(questionsList.children[index].cloneNode(true));
-        }
-        labels.appendChild(labelsList.children[index].cloneNode(true))
-
-        if (index === questionsList.children.length - 1) {
-            nextButton.innerHTML = "Submit";
-            nextButton.addEventListener('click', infoSubmit)
-        }
-
-        backButton.style.opacity = 1;
-
-    }
-};
-//let answers = {age: "", gender: "", race: "", heightFt: "", heightIn: "", weight: "", state: "", diabetes: "", smoking: "", cholesterol: "", excercise: ""};
+		index++;
+		
+		wipeQuestions();
+		updateQuestion();
+		revealBackButton();
+		
+		if (index === questionsList.children.length - 1) {
+			nextToSubmit();
+		}
+	}
+}
 
 async function infoSubmit() {
-    console.log(answers);
+	console.log(answers);
+}
 
-    //Send a POST request to localhost://8080 with all of the answers included as a form.
-    // let res = await axios.post("http://localhost:8080", {
-    //     age: answers.age,
-    //     gender: answers.gender,
-    //     race: answers.race,
-    //     heightFt: answers.heightFt,
-    //     heightIn: answers.heightIn,
-    //     weight: answers.weight,
-    //     state: answers.state,
-    //     diabetes: answers.diabetes,
-    //     smoking: answers.smoking,
-    //     cholesterol: answers.cholesterol,
-    //     excercise: answers.excercise
-    // })
-    // .then(function (res) {console.log(res.data);})
-    // .catch(function (err) {console.log(err);});
-    const response = await fetch("http://localhost:8080", {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: `{
-        "age": "${answers.age}",
-        "gender": "${answers.gender}",
-        "race": "${answers.race}",
-        "heightFt": "${answers.heightFt}",
-        "heightIn": "${answers.heightIn}",
-        "weight": "${answers.weight}",
-        "state": "${answers.state}",
-        "diabetes": "${answers.diabetes}",
-        "smoking": "${answers.smoking}",
-        "cholesterol": "${answers.cholesterol}"
-        
-        }`,
-    }).then((response) => response.text())
-        .then((data) => console.log(data));;
-    // excercise: ${answers.excercise};
+hideBackButton();
+backButton.addEventListener("click", backQuestion);
+nextButton.addEventListener("click", nextQuestion);
 
-
-};
-
-backButton.style.opacity = 0;
 nextQuestion();
